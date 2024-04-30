4eeb6dc8dc3ad17224d3a77f1898c715977e59c7
Allan Kariuki <kariukiallan850@gmail.com>
2024-04-30 17:32:25 +0300

    Forum and shop intial dev

---
+++ src/app/forum/page.tsx
@@ -0,0 +1,7 @@
+export default function Forum() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>Forum</div>
+        </main>
+    );
+    }
