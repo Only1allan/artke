7104944e6f755a74776dfec6638fbcae7bcb63b1
Allan Kariuki <kariukiallan850@gmail.com>
2024-05-10 04:25:22 +0300

    Navbar and Footer 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,20 @@ export default function Footer() {
       <footer className="bg-neutral p-10 text-neutral-content">
         <div className="footer m-auto max-w-7xl">
           <div>
-            <span className="footer-title">Services</span>
-            <a className="link-hover link">Art </a>
-            <a className="link-hover link">Photography</a>
+            <span className="footer-title">Art Types</span>
+            <a className="link-hover link">Mural Art </a>
             <a className="link-hover link">Software Development</a>
-            <a className="link-hover link">Advertisment</a>
+            <a className="link-hover link">Photography</a>
+            <a className="link-hover link">WoodWork</a>
+            <a className="link-hover link">Painting</a>
           </div>
           <div>
             <span className="footer-title">Company</span>
             <a className="link-hover link">About us</a>
             <a className="link-hover link">Contact</a>
             <a className="link-hover link">Shop</a>
+            <a className="link-hover link">Forum</a>
+
           </div>
           <div>
             <span className="footer-title">Legal</span>
