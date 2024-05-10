84dc202a1bf8aac9fb00a87d8bcc7bd3d7c5d987
Allan Kariuki <kariukiallan850@gmail.com>
2024-05-11 01:25:53 +0300

    authentication api routes

---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/_db/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { envValidate } from "@/_lib/envValidate";
+
+export const authOptions : NextAuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: envValidate.GOOGLE_CLIENT_ID,
+            clientSecret: envValidate.GOOGLE_CLIENT_SECRET
+        })
+
+    ]
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST}
